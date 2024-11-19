187d800b5d36cf0fa7196e1b83a9504210945295
Paul Ling <lingzishengppl15@gmail.com>
2024-11-18 19:54:17 -0800

    Initial commit

---
+++ app/(api)/api/route.js
@@ -0,0 +1,16 @@
+import { ApolloServer } from '@apollo/server';
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
+// import { NextRequest } from 'next/server';
+
+import typeDefs from './_typeDefs/index';
+import resolvers from './_resolvers/index';
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: process.env.NODE_ENV !== 'production',
+});
+
+const handler = startServerAndCreateNextHandler(server);
+
+export { handler as GET, handler as POST };
